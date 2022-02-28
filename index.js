89482c4a70bc67b929c167584413b5a4eec4cd3e
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-02-28 09:27:56 -0800

    commit tree 8f012642b35c7dcb3b6959ab973c17502c073cb9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(target = "roller-skate"){
+  console.log(`This Saturday, I want to ${target}!`);
+  return `This Saturday, I want to ${target}!`;
+}
+saturdayFun();
+function mondayWork(place="go to the office."){
+    console.log (`This Monday, I will ${place}.`);
+    return `This Monday, I will ${place}`;
+}
+mondayWork();
+function wrapAdjective(symbol = "special"){
+  return function(target = "*"){
+   return `You are ${symbol}${target}${symbol}!`;
+  };
+ };
+ 
+ wrapAdjective("a dedicated programmer");
